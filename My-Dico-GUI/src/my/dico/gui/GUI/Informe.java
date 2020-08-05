64503cd4447155cc5cba0b343451bf47c5044242
Pablo Jj <pablojj1808@correo.ugr.es>
2020-08-05 23:17:38 +0200

    Intentando hacer un test desde las falladas

--- My-Dico-GUI/src/my/dico/gui/GUI/Informe.java
+++ My-Dico-GUI/src/my/dico/gui/GUI/Informe.java
@@ -1,20 +1,23 @@
 package my.dico.gui.GUI;
 
-import java.util.List;
+import java.util.ArrayList;
+import my.dico.gui.Modelo.Examen;
+import my.dico.gui.Modelo.Pregunta;
 
 /**
  *
  * @author Pablo Jj
  */
 public class Informe extends javax.swing.JFrame {
+    
+    private Examen e;
 
-    public Informe(List palabras) {
+    public Informe(Examen e) {
         initComponents();
+        this.e = e;
         jTextArea1.setText("Has fallado\n");
         jTextArea1.append("============\n");
-        palabras.forEach(i -> {
-            jTextArea1.append(i + "\n");
-        });
+        this.mostrarFalladas();
         setVisible(true);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(javax.swing.JFrame.HIDE_ON_CLOSE);
@@ -27,7 +30,8 @@ public class Informe extends javax.swing.JFrame {
 
         jScrollPane1 = new javax.swing.JScrollPane();
         jTextArea1 = new javax.swing.JTextArea();
-        jButton1 = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
+        jButton3 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -37,25 +41,52 @@ public class Informe extends javax.swing.JFrame {
 
         getContentPane().add(jScrollPane1, java.awt.BorderLayout.CENTER);
 
-        jButton1.setText("salir");
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
+        jButton2.setText("salir");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
+                jButton2ActionPerformed(evt);
             }
         });
-        getContentPane().add(jButton1, java.awt.BorderLayout.PAGE_END);
+        getContentPane().add(jButton2, java.awt.BorderLayout.PAGE_END);
+
+        jButton3.setText("hacer examen con estas palabras");
+        jButton3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton3ActionPerformed(evt);
+            }
+        });
+        getContentPane().add(jButton3, java.awt.BorderLayout.PAGE_START);
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
         this.dispose();
-    }//GEN-LAST:event_jButton1ActionPerformed
+    }//GEN-LAST:event_jButton2ActionPerformed
+
+    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+        this.modificarExamen();
+        new Test(e);
+    }//GEN-LAST:event_jButton3ActionPerformed
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JButton jButton3;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTextArea jTextArea1;
     // End of variables declaration//GEN-END:variables
+
+    private void mostrarFalladas() {
+        e.getPreguntas().entrySet().stream().filter(i -> (!i.getValue())).forEachOrdered(i -> {
+            jTextArea1.append(i.getKey().getPregunta() + "\n");
+        });
+    }
+
+    private void modificarExamen() {
+        ArrayList a = new ArrayList<Pregunta>();
+        e.getPreguntas().entrySet().stream().filter(i -> (!i.getValue())).forEachOrdered(i -> {
+            a.add(i.get);
+        });
+    }
 }
